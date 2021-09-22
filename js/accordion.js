e93ba843347f40762ec3fdbb0910037954755590
Igor Lyakhov <igorlyahov@gmail.com>
2021-09-23 01:09:22 +1000

    SmoothScroll и Табы
    Добавляет плавную анимацию при скроле и табы

--- js/accordion.js
+++ js/accordion.js
@@ -3,19 +3,17 @@ const btns = document.querySelectorAll('.feature__link')
 
 
 
-btns.forEach((btnItem, index) => {
-    btnItem.addEventListener('click', () => {
-        btns.forEach((btnItem) => {
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        btns.forEach((btnItem, idx) => {
+            if (btnItem === btn) {
+                btnItem.classList.toggle('feature__link_active')
+                lists[idx].classList.toggle('hidden')
+            } else {
                 btnItem.classList.remove('feature__link_active')
-            }) //перебераем все кнопки и активный класс
-
-        btnItem.classList.add('feature__link_active') //добавляем активный класс для кликнутой кнопки
-
-        lists.forEach((listItem) => {
-                listItem.classList.add('hidden')
-            }) //перебераем список и присваиваем класс hidden
-
-        lists[index].classList.remove('hidden') //элементу index удаляем класс hidden
+                lists[idx].classList.add('hidden')
+            }
+        })
 
     })
 })
